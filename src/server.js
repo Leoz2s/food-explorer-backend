912efb901f7ea5c3f79e0d46964f305beb8de8f9
Leonardo <leonardosilvasantosss22@gmail.com>
2024-05-05 06:08:42 -0300

    Fixed issue with Cookie

--- src/server.js
+++ src/server.js
@@ -9,14 +9,6 @@ app.use(express.json());
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-// app.use((request, response, next) => {
-//   response.header('Access-Control-Allow-Credentials', 'true');
-//   response.header('Access-Control-Allow-Origin', "http://localhost:5173");
-//   response.header('Access-Control-Allow-Methods', 'GET,HEAD,PATCH,DELETE,POST,PUT');
-//   response.header('Access-Control-Allow-Credentials', 'Origin');
-//   next();
-// });
-
 const cors = require("cors");
 app.use(cors({
   origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://food-explorer-frontend-leo.netlify.app"],
