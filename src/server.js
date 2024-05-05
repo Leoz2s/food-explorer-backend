9011e64a4e776adc29a424a2dafd4411654de9c2
Leonardo <leonardosilvasantosss22@gmail.com>
2024-05-05 05:23:58 -0300

    HTTP header fix

--- src/server.js
+++ src/server.js
@@ -9,29 +9,25 @@ app.use(express.json());
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-app.use((request, response, next) => {
-  response.header('Access-Control-Allow-Credentials', 'true');
-  response.header('Access-Control-Allow-Origin', "http://localhost:5173");
-  response.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,PATCH,DELETE,POST,PUT');
-  response.header('Access-Control-Allow-Credentials', `X-CSRF-Token, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version,
-    Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method,
-    Access-Control-Request-Headers`);
-  next();
-});
+// app.use((request, response, next) => {
+//   response.header('Access-Control-Allow-Credentials', 'true');
+//   response.header('Access-Control-Allow-Origin', "http://localhost:5173");
+//   response.header('Access-Control-Allow-Methods', 'GET,HEAD,PATCH,DELETE,POST,PUT');
+//   response.header('Access-Control-Allow-Credentials', `X-CSRF-Token, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version,
+//     Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method,
+//     Access-Control-Request-Headers`);
+//   next();
+// });
 
 const cors = require("cors");
 app.use(cors({
   origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://food-explorer-frontend-leo.netlify.app"],
   credentials: true,
   source: '/:path*',
-  headers: [
-    { key: 'Access-Control-Allow-Credentials', value: 'true' },
-    { key: 'Access-Control-Allow-Origin', value: '*' },
-    { key: 'Access-Control-Allow-Methods', value: 'GET,HEAD,OPTIONS,PATCH,DELETE,POST,PUT' },
-    { key: 'Access-Control-Allow-Headers', value: `X-CSRF-Token, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version,
-      Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method,
-      Access-Control-Request-Headers` },
-  ],
+  methods: ['GET', 'HEAD', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
+  allowHeaders: ['X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'X-Api-Version', 'Authorization',
+    'Access-Control-Allow-Headers', 'Origin', 'Accept', 'X-Requested-With', 'Content-Type', 'Access-Control-Request-Method',
+    'Access-Control-Request-Headers'],
 }));
 
 const routes = require("./routes");
