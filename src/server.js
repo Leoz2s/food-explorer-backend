b52b0539d72d4fde3198f71aaf0daeae00c1cc1f
Leonardo <leonardosilvasantosss22@gmail.com>
2024-05-05 00:28:49 -0300

    CORS config for Deploy

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 const cors = require("cors");
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://api-foodexplorer-6hcw.onrender.com"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://food-explorer-frontend-leo.netlify.app"],
   credentials: true,
 }));
 
